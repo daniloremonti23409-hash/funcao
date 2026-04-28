8753c5268cdfc096a47664b37030ce2f4008d68c
daniloremonti23409-hash <daniloremonti23409@gmail.com>
2026-04-27 22:07:35 -0300

    atividade edu função finalizada

--- atividade_funcao.php
+++ atividade_funcao.php
@@ -110,5 +110,249 @@ return $invertido;
 echo inverter("Carambolas");
 echo "<br>";
 
+?><br><br>
+// exercicio 6 //<br><br>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  
+<form action="" method="post">
+<label for="">Valor da compra</label><br><br>
+<input type="number" name="valor"> digite o valor aqui<br><br>
+<button type="submit">Comprar</button><br><br>
+
+</form>
+</body>
+</html>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {    
+
+    $valor = $_POST["valor"];
+
+    function descontoCompra($valor) {
+
+        if ($valor >= 100 && $valor <= 299) {
+            return "Desconto de 10%!<br>O valor final é " . ($valor - ($valor * 0.1));
+
+        } elseif ($valor >= 300 && $valor <= 499) {
+            return "Desconto de 15%!<br>O valor final é " . ($valor - ($valor * 0.15));
+
+        } elseif ($valor >= 500) {
+            return "Desconto de 20%!<br>O valor final é " . ($valor - ($valor * 0.20));
+
+        } else {
+            return "Sem desconto.";
+        }
+    }
+
+    echo descontoCompra($valor);
+}
+
 ?>
 
+<br><br>
+// exercicio 7 //<br>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="" method="post">
+<label for="">Gerar login</label><br><br>
+<label for=""> nome</label><br><br>
+<input type="text" name="nome"><br><br>
+<label for=""> sobrenome</label><br><br>
+<input type="text" name="sobrenome"><br><br>
+<button type="submit">Gerar login </button>
+
+</form>
+
+
+</body>
+</html>
+
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+$nome = $_POST["nome"];
+$sobrenome = $_POST["sobrenome"];
+
+function gerarLogin($nome,$sobrenome){
+return ($nome. "." . $sobrenome) ;
+
+}
+
+echo gerarLogin($nome,$sobrenome);
+}
+
+
+?>
+
+<br>
+// exercicio 8 //<br><br>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="" method="post">
+<label for="">Valor de compra</label>
+<input type="number" name="frete">
+<button type="submit">Calcular frete</button>
+
+
+</form>
+
+</body>
+</html>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+$frete = $_POST["frete"];
+
+
+function MostrarFrete($frete){
+if($frete <= 100){
+return "Até R$ 100 :Valor do frete será: R$ 20,00<br>";
+
+}
+elseif($frete >=101 && $frete <=300){
+return "Até R$ 300: Valor do frete será: R$ 15,00<br>";
+
+}elseif($frete > 300){
+return "Você ganhou frete grátis!";
+
+}
+else{
+return "nenhum valor encontrado!";
+
+}
+
+}
+
+echo MostrarFrete($frete);
+
+}
+
+?>
+<br><br>
+// exercicio 09 //<br>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="" method="post">
+<label for="">Digitar senha</label>
+<input type="text" name="senha">
+<button type="submit">Testar senha</button>
+
+</form>
+
+
+</body>
+</html>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+$senha = $_POST["senha"];
+
+
+function validarSenha($senha){
+
+if(strlen($senha) < 8){
+return "A senha deve possuir pelo menos 8 caracteres!";
+
+}
+if(!preg_match('/[0-9]/',$senha)){
+return "a senha deve possuir numeros!";
+
+}
+if(!preg_match('/[A-Z]',$senha)){
+return "a senha deve possuir letras maiusculas!";
+
+}
+return "senha invalida";
+}
+
+
+
+echo validarSenha($senha);
+}
+
+?>
+<br><br>
+// exercicio 10//<br><br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="" method="post">
+<label for="">Gerar token </label>
+
+<button type="submit" name="tamanho">Gerar</button>
+
+</form>
+
+
+</body>
+</html>
+
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+
+$tamanho = $_POST["tamanho"];
+
+
+function gerarToken($tamanho = 10){
+    $caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $token = '';
+
+    for($i = 0; $i < $tamanho; $i++){
+        $token .= $caracteres[rand(0, strlen($caracteres) - 1)];
+    }
+
+    return $token;
+}
+
+// Gerando e exibindo o token
+$tokenGerado = gerarToken(10);
+echo "Token gerado: " . $tokenGerado;
+}
+?>
